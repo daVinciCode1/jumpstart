22d6147a84e557e4a79b4d122095a6154d7c9bf1
Ludwig Wilke <ludwig.wilke98@gmail.com>
2017-07-20 10:15:03 +0200

    initialise slick carousel, bugs

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000;
+    arrows: false;
+    draggable: false;
+    pauseOnFocus: false;
+    pauseOnHover: false;
+    slidesPerRow: 4;
+    slidesToShow: 1;
+  });
+});
